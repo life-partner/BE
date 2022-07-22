0a5fe028e7d5e6de2d078e69e2654e91917fed16
tak-shability <tak@shability.io>
2022-07-22 17:44:09 +0900

    add: cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 require('dotenv').config();
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import user_router from './routes/user';
 import article_router from './routes/article';
 import partner_router from './routes/partner';
@@ -12,6 +13,13 @@ db.connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
 
 app.use('/api', [user_router, article_router, partner_router]);
 
